3a4362ce2b5e369862bd4e81a04a892c98952d4d
Fabian Altahona <fabian.altahona@koombea.com>
2014-08-15 09:39:14 -0500

    Stop the animation if the user scrolls manually (only during the animation)

--- scrollToSection.js
+++ scrollToSection.js
@@ -1,16 +1,27 @@
 //
 //  jQuery Scroll To Section Plugin by Andres Rojas (andresroberto2009@gmail.com)
+(function ($) {
+  $.fn.scrollToSection = function(options){
 
-$.fn.scrollToSection = function(options){
-    
-    function scrollToSection(id, time){
-      $("body,html").animate({scrollTop: $(id).offset().top}, time);
-    }
+    this.options = options || {};
 
-    $(this).on("click", "a", function(event) {
-      event.preventDefault();
-      var sectionName = $(this).attr("href");
-      scrollToSection( sectionName, 1000);
-    });
+      function scrollToSection(id, time){
+        var topMargin = options.topMargin || 0,
+            $root     = $("html, body");
 
-};
+        $root.on("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+          $root.stop();
+        });
+        $root.animate({scrollTop: $(id).offset().top - topMargin}, time, function(){
+          $root.off("scroll mousedown DOMMouseScroll mousewheel keyup");
+        });
+      }
+
+      $(this).on("click", "a", function(event) {
+        event.preventDefault();
+        var sectionName = $(this).attr("href");
+        scrollToSection( sectionName, 1000);
+      });
+
+  };
+})(jQuery);
